2f5d9855230981190567674b91b6da3b61687e4a
Henrique Romero <luishenriquebuenoromero@gmail.com>
2024-04-23 13:34:47 +0000

    build a lot of things

---
+++ app/src/main/java/application/controller/GeneroController.java
@@ -0,0 +1,20 @@
+package application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import application.repository.GeneroRepository;
+
+@Controller
+@RequestMapping("/generos")
+public class GeneroController {
+    @Autowired
+    private GeneroRepository generoRepo;
+
+    @RequestMapping("/insert")
+    public String insert() {
+        return "/genero/insert";
+    }
+}
